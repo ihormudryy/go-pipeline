bba55b93ef0cc79a3e2dfe8072cd43782077ec60
Ihor <ihor.mudryy@here.com>
2018-09-19 14:32:35 +0200

    Added config yaml files

--- script.sh
+++ script.sh
@@ -4,11 +4,8 @@ aws ec2 run-instances --image-id ami-3448784b \
 --security-group-ids sg-7c1a6206 \
 --subnet-id subnet-3728cf7e \
 --tag-specifications \
-'ResourceType=string,Tags=[{Key=Name,Value=K8S Pod}]' \
-'ResourceType=string,Tags=[{Key=Project,Value=I_EXT_OLPD_ADMINS}]' \
-'ResourceType=string,Tags=[{Key=Team,Value=OLP Delivery}]' \
-'ResourceType=string,Tags=[{Key=createdBy,Value=mudryy}]' \
-'ResourceType=string,Tags=[{Key=Owner,Value=OLP_Delivery_Systems_Engineering}]'
+'ResourceType=instance,Tags=[{Key=Owner,Value=OLP_Delivery_Systems_Engineering},{Key=Name,Value=K8S Pod},{Key=Project,Value=I_EXT_OLPD_ADMINS},{Key=Team,Value=OLP Delivery},{Key=createdBy,Value=mudryy}]' \
+--dry-run
 
 kubeadm init --token=102952.1a7dd4cc8d1f4cc5 --kubernetes-version $(kubeadm version -o short)
 kubectl apply -f https://raw.githubusercontent.com/kubernetes/dashboard/master/src/deploy/recommended/kubernetes-dashboard.yaml
